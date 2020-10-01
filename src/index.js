d95c4e5eaf8e321d52e0e43055363d46d99c9c33
snegurova <snegurova@gmail.com>
2020-10-01 23:11:31 +0300

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!Array.isArray(array) || !array.length) {
+        return 0;
+    }
+    return array.sort((a, b) => a - b)[0];
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!Array.isArray(array) || !array.length) {
+        return 0;
+    }
+    return array.sort((a, b) => b - a)[0];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || !array.length) {
+        return 0;
+    }
+    return array.reduce( (sum, item) => sum + item ) / array.length;
 }
